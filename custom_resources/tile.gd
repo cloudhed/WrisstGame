bf9687b7a5d409748f0a2f7f29e876dce0281df9
Isak <103982406+cloudhed@users.noreply.github.com>
2025-03-28 22:25:20 +0100

    Second part of large commit

---
+++ custom_resources/tile.gd
@@ -0,0 +1,13 @@
+class_name Tile
+extends Resource
+
+enum Type{ATTACK, DEFEND, POWER, DEBUFF, BUFF, FAIL}
+enum Target {SELF, SINGLE_ENEMY, ALL_ENEMIES, EVERYONE}
+
+@export_group("Tile Attributes")
+@export var id: String
+@export var type: Type
+@export var target: Target
+
+func is_single_targeted() -> bool:
+	return target == Target.SINGLE_ENEMY
